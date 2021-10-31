c011c9cd50e43f05e7067fb8ebefe7c238086cc6
Mandy Liesch <amliesch@ncsu.edu>
2021-10-31 08:00:50 -0400

    Yan 1031

--- automation_create_channels.R
+++ automation_create_channels.R
@@ -1,6 +1,6 @@
 # author: Yan Liu
 # date: 10/30/2021
-# purpose: Render project2.Rmd as several .md files for repo.
+# purpose: Render 558project2.Rmd as several .md files for repo.
 
 
 channels<-c("lifestyle","entertainment","bus","tech","lifestyle","world","socmed")
@@ -12,7 +12,7 @@ reports<-tibble(output_file,params)
 library(rmarkdown)
 apply(reports, MARGIN=1, 
       FUN=function(x){
-        render(input="auto_test.Rmd",output_file=x[[1]], params=x[[2]], clean = TRUE,encoding = "UTF-8")
+        render(input="558project2.Rmd",output_file=x[[1]], params=x[[2]], clean = TRUE,encoding = "UTF-8",output_options = list(toc=TRUE, toc_depth = 3, html_preview=FALSE))
         })
 
 
